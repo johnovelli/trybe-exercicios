9d438d18e20db6ba00b84f4df9d37aac5136b4ca
George Novelli Tarlazis <joh.novelli90@gmail.com>
2023-05-02 15:54:25 -0300

    fundamentos-03/01/3

---
+++ 01 fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/Exercicio3/scripts.js
@@ -0,0 +1,14 @@
+let status ="aprovada";
+
+switch(status) {
+  case "aprovada":
+    console.log("Parabensssss,uhulllll");
+    break;
+    case "reprovada":
+      console.log("Shame on youuuuu");
+      break;
+
+      default:
+      console.log("Nada a declarar");
+
+}
