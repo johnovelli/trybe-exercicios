79a507b85b1d0459d99b1df1a3c2da191dade826
George Novelli Tarlazis <joh.novelli90@gmail.com>
2023-05-04 18:26:02 -0300

    fundamentos/04/03-desafios

---
+++ 01 fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/aula.js
@@ -0,0 +1,5 @@
+function teste (number1, number2) {
+  return number1 + number2;
+}
+let result = teste(10, 10);
+console.log(result);
---
+++ 01 fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/parametros.js
@@ -0,0 +1,13 @@
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+// escreva aqui sua função
+
+function adicionar(nome) {
+  clientesTrybeBank.push(nome);
+  return "Cliente adicionado"
+}
+console.log(adicionar('Alencara'));
+console.log('Ola');
+
+
+
